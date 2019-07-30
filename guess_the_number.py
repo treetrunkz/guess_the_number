32616d6f0c14d478e806ce16f76d9195bd25857d
Joanna Folk <36090631+joannamfolk@users.noreply.github.com>
2019-07-29 20:13:20 -0700

    Update guess_the_number.py
    I haven't figured out classes yet, so until then I want to keep my code clean.  What I truly wanted to demonstrate was branching, and no bold-finesse will show that better than the actual acts.

--- guess_the_number.py
+++ guess_the_number.py
@@ -14,10 +14,8 @@ userName = input('Hello!  What is your name? ')
 print()
 
 # to explain the logic of the game with emphasis on inputs
-print('Well ' + str('\033[1m{}\033[0m'.format(userName.title())) + ', I am thinking of a number between' + '\033[1m{}\033[0m'.format(' 1 ')
-      + 'and' + '\033[1m{}\033[0m'.format(' 20.') + '\nYou can ask for a hint by typing \''
-      + '\033[1m{}\033[0m'.format('hint') + '\' or type'
-      + ' \'' + '\033[1m{}\033[0m'.format('exit') + '\' if you\'re done playing.')
+print('Well ' + userName.title() + ', I am thinking of a number between 1 and 20.')
+print('You can ask for a hint by typing \'hint\' or type \'exit\' if you\'re done playing.')
 
 # create the random number the player must guess
 computerNumber = random.randint(1, 20)
@@ -30,15 +28,14 @@ while userNumber != computerNumber:
 
     # to create an exit situation with option to play another match
     if userNumber.lower() == 'exit':
-        print('You played a good game! The number I chose was: ' + str('\033[1m{}\033[0m'.format(computerNumber)))
+        print('You played a good game! The number I chose was: ' + str(computerNumber))
         playAgain = input('Would you like to play another match? (y/n)')
         playAgain = playAgain.lower() # to help with case match
 
         # for yes to play another game
         if playAgain == 'y' or playAgain == 'yes':
             print()
-            print('Alright, let\'s play another game:\nI\'m thinking of a number between ' +
-                  '\033[1m{}\033[0m'.format('1') + ' and ' + '\033[1m{}\033[0m'.format('20'))
+            print('Alright, let\'s play another game:\nI\'m thinking of a number between 1 and 20.')
             roundsPlayed = 0 # to reset variables
             computerNumber = random.randint(1, 20)
 
@@ -69,16 +66,16 @@ while userNumber != computerNumber:
                     print('The number is somewhere below five.')
 
                 elif computerNumber > 4 and computerNumber < 11:
-                    print('The number is somewhere between 5 and 10')
+                    print('The number is somewhere between 5 and 10.')
 
                 elif computerNumber > 9 and computerNumber < 16:
-                    print('The number is somewhere between 10 and 15')
+                    print('The number is somewhere between 10 and 15.')
 
                 elif computerNumber >= 15:
                     print('The number is somewhere between 15 and 20.')
 
         else:
-            print('Let\'s get back to it!\nI\'m thinking of a number between 1 and 20')
+            print('Let\'s get back to it!\nI\'m thinking of a number between 1 and 20.')
 
 
     # to cover bases if user inputs number too high or too low
@@ -95,17 +92,13 @@ while userNumber != computerNumber:
 
     # if the number guessed is identical to the computer number
     else:
-        print('Great job ' + '\033[1m{}\033[0m'.format(userName.title()) +
-        '!  You guessed my number in ' + '\033[1m{}\033[0m'.format(roundsPlayed) + ' round(s)!')
+        print('Great job ' + userName.title() + '!  You guessed my number in ' + str(roundsPlayed) + ' round(s)!')
         anotherRound = input('Would you like to play another match? (y/n): ') # to play another round
 
         # if yes then the game restarts
         if anotherRound.lower() == 'y' or anotherRound.lower() == 'yes':
-            print('\nAlright, let\'s go!  I\'m thinking of another number between '
-                  + '\033[1m{}\033[0m'.format('1') + ' and ' + '\033[1m{}\033[0m'.format('20'))
-
-            print('Don\'t forget you can always type \'' + '\033[1m{}\033[0m'.format('exit')
-                  + '\' or \'' + '\033[1m{}\033[0m'.format('hint') + '\' if you\'re stuck.')
+            print('\nAlright, let\'s go!  I\'m thinking of another number between 1 and 20.')
+            print('Don\'t forget you can always type \'exit\' or \'hint\' if you\'re stuck.')
             roundsPlayed = 0 # to reset variables
             computerNumber = random.randint(1, 20)
 
